176a6e91990f805a7d1dfefae1fbb6099089ebd1
Phil Ware <phil.ware@u-blox.com>
2024-01-19 19:14:47 +0000

    Added message output to what ttyUSBx is actually attached to the ttyEVK symlink

--- setTTY2EVK.sh
+++ setTTY2EVK.sh
@@ -2,9 +2,11 @@
 rm /dev/ttyEVK
 if [ "$(ls /dev/ttyUSB* | wc -l)" -eq "4" ]; then
     echo "Found new Rev EVK with four UARTs."
+    echo "ttyEVK ==> ttyUSB2"
     ln -s /dev/ttyUSB2 /dev/ttyEVK
 elif [ "$(ls /dev/ttyUSB* | wc -l)" -eq "2" ]; then
     echo "Found old Rev EVK with two UARTs."
+    echo "ttyEVK ==> ttyUSB0"
     ln -s /dev/ttyUSB0 /dev/ttyEVK
 else
     echo "No ttyUSB ports found!!"
