3c94f5793c68e96222334faeef8c54e54a13f4d5
philwareublox <phil.ware@u-blox.com>
2024-01-24 12:03:17 +0000

    typo

--- cellular_tracker/src/main.c
+++ cellular_tracker/src/main.c
@@ -310,14 +310,14 @@ int main(int arge, char *argv[])
 
     // The Network registration task is used to connect to the cellular network
     // This will monitor the +CxREG URCs
-    printWarn("STARTING NETWORK REGISTRATION");
+    printInfo("Starting network registration...");
     if (runTask(NETWORK_REG_TASK, networkIsUp) != U_ERROR_COMMON_SUCCESS)
         finalize(ERROR);
 
     // The MQTT task connects and reconnects to the MQTT broker selected in the 
     // config.h file. This needs to run for MQTT messages to be published and
     // for remote control messages to be handled
-    printWarn("STARTING MQTT SYSTEM");
+    printInfo("Starting MQTT...");
     if (runTask(MQTT_TASK, mqttConnectionIsUp) != U_ERROR_COMMON_SUCCESS)
         finalize(ERROR);
 
